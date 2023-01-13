eaa93bc45d1225da5c90f1872291239691ad3c4e
Rhael Henrique <59274131+rhaelhenrique@users.noreply.github.com>
2023-01-13 14:34:26 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import axios from "axios";
+
+//Exemplo01
+//fetch("https://api.github.com/users/rhaelhenrique").then((response) => {
+//  response.json().then((data) => console.log(data));
+//});
+
+//Exemplo02
+//axios.get("https://api.github.com/users/rhaelhenrique").then((res) => {
+//  console.log(res.data.login, res.data.id);
+//  let nome = res.data.login;
+//  let id = res.data.id;
+//  console.log("Nome: "+nome+" ID: "+id)
+//}).catch(error => console.log(error.message))
+
+//Exemplo03
